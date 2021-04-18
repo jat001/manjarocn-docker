f95e90efef6e08114d047fcdc969efc627bc9473
Jat <jat@sinosky.org>
2021-04-19 01:01:04 +0800

    update makepackage

--- makepackage.sh
+++ makepackage.sh
@@ -17,12 +17,14 @@ mkdir -p "$pkg_root" /build/sources /build/srcpackages
 cp -R /gpg /home/builder/.gnupg
 chown -R builder:builder /build /home/builder
 
-[ "${PACKAGER:-}" ] && sudo -u builder gpg --list-secret-keys "$PACKAGER"
-[ "${GPGKEY:-}" ] && sudo -u builder gpg --list-secret-keys "$GPGKEY"
-sed -Ei "/^#PACKAGER/ { s/^#//; s/=.*/='$PACKAGER'/ }; /^#GPGKEY/ { s/^#//; s/=.*/='$GPGKEY'/ }" /etc/makepkg.conf
+sudo -u builder gpg --list-secret-keys "$PACKAGER" >/dev/null
+sudo -u builder gpg --list-secret-keys "$GPGKEY" >/dev/null
+sed -Ei "/^#PACKAGER/ { s/^#//; s/=.*/='$PACKAGER'/ };
+/^#GPGKEY/ { s/^#//; s/=.*/='$GPGKEY'/ };
+" /etc/makepkg.conf
 
 sudo -u builder gpg --armor --export "$GPGKEY" | pacman-key --add /dev/stdin
-pacman-key --lsign-key "$GPGKEY"
+pacman-key --lsign-key "$GPGKEY" &>/dev/null
 
 find "$pkg_root" -name '*.pkg.tar.zst' -printf "$pkg_cache_root/%f\n" | xargs rm -f
 
@@ -30,28 +32,33 @@ find "$pkg_root" -name '*.pkg.tar.zst' -printf "$pkg_cache_root/%f\n" | xargs rm
 [ "${UPDATEMIRRORS:-0}" -gt 0 ] && pacman-mirrors --geoip
 pacman -Syyuu --noconfirm --noprogressbar
 
+function getvar () {
+    varname="$1"
+    required=${2:-0}
+
+    varval=$(set +x; source PKGBUILD >/dev/null; set +u; echo ${!varname} | xargs)
+    [ $required -ne 0 ] && [ -z "$varval" ] && return 1
+    echo "$varval"
+}
+
 cd /build/workspace
-pkgname=$(source PKGBUILD && echo "$pkgname" | xargs)
 
-# update $pkgver
-# [ "$(source PKGBUILD && type -t pkgver)" == 'function' ] && sudo -u builder makepkg --cleanbuild --nodeps --nobuild --noprepare
+pkgname="$(getvar 'pkgname' 1)"
+pkgver="$(getvar 'pkgver' 1)"
+pkgrel="$(getvar 'pkgrel' 1)"
 
-pkgver="$(source PKGBUILD && echo "$pkgver" | xargs)"
-[ "$pkgver" ] || exit 1
-pkgrel=$(source PKGBUILD && echo "$pkgrel" | xargs)
-[ "$pkgrel" ] || exit 1
 pkgver="$pkgver-$pkgrel"
-epoch=$(set +u; source PKGBUILD && echo "$epoch" | xargs)
-[ "$epoch" ] && pkgver="$epoch:$pkgver"
+epoch=$(getvar 'epoch')
+[ -n "$epoch" ] && pkgver="$epoch:$pkgver"
 
 # `pacman -Si` returns 1 if package not in sync database
 repover=$(pacman -Si "$pkgname" | grep -Ei '^version' | cut -d':' -f'2-' | xargs) \
     && [ "$(vercmp "$repover" "$pkgver")" -ge 0 ] && exit 0
 
 [ "${UPDATESUMS:-0}" -gt 0 ] && sudo -u builder updpkgsums
-gpg_keys=$(set +u; source PKGBUILD && echo "${validpgpkeys[@]}" | xargs)
-[ "$gpg_keys" ] && sudo -u builder gpg --recv-keys $gpg_keys
+gpg_keys=$(getvar 'validpgpkeys[@]')
+[ -n "$gpg_keys" ] && sudo -u builder gpg --recv-keys $gpg_keys
 
-sudo -u builder makepkg --cleanbuild --clean --force --syncdeps --noconfirm --noprogressbar --needed
+sudo -u builder "SRCDEST=/build/sources/$pkgname" makepkg --cleanbuild --clean --force --syncdeps --noconfirm --noprogressbar --needed
 sudo -u builder repo-add --new --remove --sign --key "$GPGKEY" "$pkg_db" "$pkg_root/$pkgname-$pkgver-$ARCH.pkg.tar.zst"
 rm -f "$pkg_root/"*.old "$pkg_root/"*.old.sig
