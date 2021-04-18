d6147a851d457946226bdd1768512a919252e32e
Jat <jat@sinosky.org>
2021-04-19 01:16:18 +0800

    do not sign db

--- makepackage.sh
+++ makepackage.sh
@@ -28,7 +28,7 @@ pacman-key --lsign-key "$GPGKEY" &>/dev/null
 
 find "$pkg_root" -name '*.pkg.tar.zst' -printf "$pkg_cache_root/%f\n" | xargs rm -f
 
-[ -f "$pkg_db" ] || sudo -u builder repo-add --sign --key "$GPGKEY" "$pkg_db"
+[ -f "$pkg_db" ] || sudo -u builder repo-add "$pkg_db"
 [ "${UPDATEMIRRORS:-0}" -gt 0 ] && pacman-mirrors --geoip
 pacman -Syyuu --noconfirm --noprogressbar
 
@@ -60,5 +60,5 @@ gpg_keys=$(getvar 'validpgpkeys[@]')
 [ -n "$gpg_keys" ] && sudo -u builder gpg --recv-keys $gpg_keys
 
 sudo -u builder "SRCDEST=/build/sources/$pkgname" makepkg --cleanbuild --clean --force --syncdeps --noconfirm --noprogressbar --needed
-sudo -u builder repo-add --new --remove --sign --key "$GPGKEY" "$pkg_db" "$pkg_root/$pkgname-$pkgver-$ARCH.pkg.tar.zst"
+sudo -u builder repo-add --new --remove "$pkg_db" "$pkg_root/$pkgname-$pkgver-$ARCH.pkg.tar.zst"
 rm -f "$pkg_root/"*.old "$pkg_root/"*.old.sig
