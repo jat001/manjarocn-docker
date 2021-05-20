9f50d9306c9a1fe624c2c711fff44c05cb9cfc0d
Jat <jat@sinosky.org>
2021-05-20 12:51:05 +0800

    add PUSH flag

--- build.sh
+++ build.sh
@@ -8,14 +8,18 @@ set -x
 : "${REPO:=manjarocn/base}"
 : "${BRANCH:=stable}"
 : "${TAG:=$(date -u +'%Y%m%d')}"
+: "${PUSH:=1}"
 
 DOCKER_BUILDKIT=1 docker build --progress=plain --pull --no-cache --squash --compress \
     --build-arg "BRANCH=$BRANCH" -t "$REPO:$BRANCH-$TAG" .
 docker tag "$REPO:$BRANCH-$TAG" "$REPO:$BRANCH-latest"
-docker push "$REPO:$BRANCH-$TAG"
-docker push "$REPO:$BRANCH-latest"
+
+if [ "$PUSH" -gt 0 ]; then
+    docker push "$REPO:$BRANCH-$TAG"
+    docker push "$REPO:$BRANCH-latest"
+fi
 
 if [ "$BRANCH" == 'stable' ]; then
     docker tag "$REPO:$BRANCH-$TAG" "$REPO:latest"
-    docker push "$REPO:latest"
+    [ "$PUSH" -gt 0 ] && docker push "$REPO:latest"
 fi
